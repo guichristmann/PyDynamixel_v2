54e82b7dede791cfbdfbf6f18bbeeef111bb0454
guichristmann <guichristmann@gmail.com>
2020-12-26 15:47:40 +0800

    Updated example to accept command line argument.

--- examples/example_Xseries.py
+++ examples/example_Xseries.py
@@ -1,32 +1,44 @@
 import PyDynamixel as pd
 from time import sleep
 import math
+import sys
+
+n = sys.argv[1]
+print(f"Will open at USB{n}")
 
 pd.setDefaultCtrlTable("X-SERIES")
 
-port = pd.DxlComm("/dev/ttyUSB0", baudrate=2000000, protocol_version=2)
+#port = pd.DxlComm("/dev/ttyUSB" + n, baudrate=2000000, protocol_version=2)
+port = pd.DxlComm("/dev/ttyUSB" + n, baudnum=0, protocol_version=2)
 
 #dyn = pd.Joint(21)
 joints = [pd.Joint(20), pd.Joint(21)]
 port.attach_joints(joints)
 
+print("Enabling torques.")
 port.enable_torques()
 
-joints[0].set_goal_value(0.0)
+print("Set goal")
+joints[0].set_goal_value(math.pi)
+joints[1].set_goal_value(math.pi)
+print("Sync write")
 port.sync_write_goal_position()
 sleep(2)
-joints[0].set_goal_value(math.pi/2)
+print("Set goal")
+joints[0].set_goal_value(math.pi + math.pi/4)
+joints[1].set_goal_value(math.pi + math.pi/4)
+print("Sync write")
 port.sync_write_goal_position()
 sleep(2)
 joints[0].set_goal_value(math.pi)
+joints[1].set_goal_value(math.pi)
+print("Sync write")
 port.sync_write_goal_position()
 sleep(2)
-joints[0].set_goal_value(math.pi + math.pi/2)
+joints[0].set_goal_value(math.pi - math.pi/4)
+joints[1].set_goal_value(math.pi - math.pi/4)
+print("Sync write")
 port.sync_write_goal_position()
-sleep(2)
-joints[0].set_goal_value(2*math.pi - 0.05)
-port.sync_write_goal_position()
-sleep(2)
 '''
 while True:
     for j in port.joints:
